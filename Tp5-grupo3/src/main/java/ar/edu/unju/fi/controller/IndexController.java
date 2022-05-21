0c6358fa181c6159966834b59949e6d23a0b891a
PC <PC@MEGATRON>
2022-05-21 14:21:54 -0300

    ea

--- Tp5-grupo3/src/main/java/ar/edu/unju/fi/controller/IndexController.java
+++ Tp5-grupo3/src/main/java/ar/edu/unju/fi/controller/IndexController.java
@@ -3,13 +3,12 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ar.edu.unju.fi.model.Alumno;
 
 @Controller
 public class IndexController {
 @GetMapping("/inicio")
-public Model getIndexPage(Model model) {
-	Alumno unAlumno = new Alumno("carlos", "gutierrez", "carlos-guti.com");
+public String getIndexPage() {
+	
 		return "Index";
 	}
 		
