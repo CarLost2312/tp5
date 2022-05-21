08a4785a9fe1ebeb56bc5ed7a2d8bf8a61d2e784
PC <PC@MEGATRON>
2022-05-21 10:39:39 -0300

    aver

---
+++ Tp5-grupo3/src/main/java/com/example/demo/Tp5Grupo3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Grupo3Application.class, args);
+	}
+
+}
