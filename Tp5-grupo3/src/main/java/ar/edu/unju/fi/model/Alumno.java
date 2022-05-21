05285a2f526bdb5a3eff150f3dc35aa5cd9aabeb
PC <PC@MEGATRON>
2022-05-21 14:20:50 -0300

    2

---
+++ Tp5-grupo3/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.fi.model;
+
+public class Alumno {
+private String nombre;
+private String apellido;
+private String email;
+
+public Alumno(String nombre, String apellido, String email) {
+	super();
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.email = email;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+	
+
+
+}
